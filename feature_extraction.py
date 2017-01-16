ef23384c1081ec2a7ea791c446827f1edb2b38c5
Jeremy Shannon <jeremyshannon@Jeremys-MacBook-Pro.local>
2017-01-15 20:07:52 -0600

    feature extraction complete

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,11 @@
 import pickle
 import tensorflow as tf
+tf.python.control_flow_ops = tf
 # TODO: import Keras layers you need here
+import numpy as np
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten
+from keras.optimizers import Adam
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -8,6 +13,8 @@ FLAGS = flags.FLAGS
 # command line flags
 flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
 flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
+flags.DEFINE_string('batch_size', '', "Batch size for model training")
+flags.DEFINE_string('epochs', '', "Number of epochs for model training")
 
 
 def load_bottleneck_data(training_file, validation_file):
@@ -46,9 +53,20 @@ def main(_):
     # the dataset
     # 10 for cifar10
     # 43 for traffic
+    batch_size = int(FLAGS.batch_size)
+    epochs = int(FLAGS.epochs)
+    num_classes = len(np.unique(y_train))
+    input_shape = X_train.shape[1:]
+    print('Classes:', num_classes, 'Input shape:', input_shape)
+    model = Sequential()
+    model.add(Flatten(input_shape=X_train.shape[1:]))
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
 
     # TODO: train your model here
-
+    model.compile('adam', 'sparse_categorical_crossentropy', ['accuracy'])
+    history = model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=epochs, shuffle=True, 
+                        validation_data=(X_val, y_val), verbose=2)
 
 # parses flags and calls the `main` function above
 if __name__ == '__main__':
